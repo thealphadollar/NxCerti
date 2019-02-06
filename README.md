1a88427ab635f6e7ccfb8420766627e4834a0b87
thealphadollar <shivam.cs.iit.kgp@gmail.com>
2019-02-06 15:27:51 +0530

    add readme

---
+++ README.md
@@ -0,0 +1,40 @@
+# NxCerti
+
+NxCerti does exactly what the name suggests; it creates certificates if provided with the base template and names.
+
+## To Use
+
+NOTE: If you have experience with programming, please look into the configuration variables present in `main.py` for better customization. The below instructions are with
+default configuration.
+
+The client uses pipenv and python imaging library for it's working.
+
+- Keep the base template image for the certificates in the project folder with the name "template.png".
+- Keep the names to be put on certificates in a csv file "names.csv" (in the project's folder) with names at O'th index with no heading.
+
+### First Run
+
+Below are the steps on how to use it **for the first time**:
+
+- `cd /to/project/directory`
+- `pip install pipenv`
+- go to project directory and run `pipenv shell --three`
+- install all dependencies
+    `pipenv install`
+- run the project and follow the CLI
+    `python3 main.py`
+
+### Consecutive Run
+
+Below are the steps that need to be followed if you've already ran the program once.
+- `cd /to/project/directory`
+- `pipenv shell`
+- `python3 main.py`
+
+The virtual environment created in first step is used here :)
+
+NOTE: All your certificates will be saved in the project directory in a folder named "certificates".
+
+## Contributing
+
+Feel free to raise issues and contribute to the project.
--- main.py
+++ main.py
@@ -1,11 +1,15 @@
 from PIL import Image, ImageFont, ImageDraw
 import csv
-
+import os
 
 """
 ONLY BELOW CONFIG TO BE MODIFIED
 """
+TEMPLATE_PATH = "template.png"  # path to the base template image
+TO_SAVE_PATH = "certificates/"  # folder to save the certificates to
 CSV_FILE = "names.csv"  # path to the csv file containing names at index 0 without heading
+HAS_HEADING = False  # True if csv file has heading row, False otherwise
+INDEX = 0  # index at which the names are stored for certificate
 FONT_SIZE = 100  # font size
 COLOR = (34, 53, 104)  # font color
 TEXT_MIDDLE_POS = (1200, 650)  # coordinates to begin writing text 
@@ -17,15 +21,20 @@ CONFIG ENDS
 
 NAMES = []
 
-temp_template = Image.open("template.png")
+temp_template = Image.open(TEMPLATE_PATH)
 template = Image.new('RGB', temp_template.size, (255, 255, 255))
 template.paste(temp_template, mask=temp_template.split()[3])
 raw_template = ImageDraw.Draw(template)
 
 with open(CSV_FILE, "r") as name_file:
     data = csv.reader(name_file)
+    if HAS_HEADING:
+        next(data, None)
     for row in data:
-        NAMES.append(row[0])
+        NAMES.append(row[INDEX])
+
+if not os.path.exists(TO_SAVE_PATH):
+    os.makedirs(TO_SAVE_PATH)
 
 """
 first argument: position
@@ -37,4 +46,4 @@ for name in NAMES:
     name_width, name_height = raw_template.textsize(name, font=FONT)
     name_pos = (TEXT_MIDDLE_POS[0]-(name_width/2), TEXT_MIDDLE_POS[1]-(name_height/2))
     raw_template.text(name_pos, name, COLOR, FONT)
-    template.save("KWoC18-"+name+".pdf", "PDF", resolution=100.0)
+    template.save(TO_SAVE_PATH+"KWoC18-"+name+".pdf", "PDF", resolution=100.0)
